d8ed34a88901913a80fdb20c28629dbced67a66e
vikassrini <1rn20cs168.tharika@gmail.com>
2025-04-21 09:21:10 +0530

    add more evals and eval criteria

--- evals/eval.py
+++ evals/eval.py
@@ -15,18 +15,27 @@ class Eval():
         return expected_output == generated_output
     
     def fuzzy_match(self,  expected_output, generated_output, threshold=80) -> bool:
-        ratio = fuzz.ratio(expected_output.lower(), generated_output.lower()) 
+        ratio = fuzz.partial_ratio(expected_output.lower(), generated_output.lower()) 
         logger.info(f"fuzzy ratio {ratio}")
         return ratio > threshold
     
-    def match_records(self,  expected_output, generated_output) -> bool:
-        if len(expected_output)==0 and len(generated_output) ==0:
+    def match_records(self,  expected_output, generated_output, threshold=90) -> bool:
+        if len(expected_output)==0 and len(generated_output)==0:
             return True
         
+        if len(expected_output) != len(generated_output):
+            return False
+        #TODO : the order of comparison matters
         expected_output=set(map(str.lower, expected_output))
         generated_output=set(map(str.lower, generated_output))
-        overlap = expected_output & generated_output
-        return len(overlap) == len(expected_output)
+        logger.info(f"expected output : {expected_output}\ngen o/p: {generated_output}")
+        
+        for (exp, gen) in zip(expected_output, generated_output):
+            score = fuzz.partial_token_set_ratio(exp.lower(), gen.lower())
+            logger.info(f"#Comparing: '{exp}' <-> '{gen}' → score: {score}")
+            if score < threshold:
+                return False
+        return True
 
     def evaluate_response(self, resume_path, ground_truth):
         logger.info(f"Processing resume: {resume_path}")
@@ -36,11 +45,14 @@ class Eval():
             "name": self.hard_match(ground_truth["name"], generated_response["name"]),
             "role": self.fuzzy_match(ground_truth["role"], generated_response["role"], 80),
             "summary_match": self.fuzzy_match(ground_truth["professional_summary"], generated_response["professional_summary"], 90),
+            # TODO: If experience gen is 20+ and actual is 28 can i consider this as success ?
             "total_experience": self.fuzzy_match(ground_truth["total_experience"], generated_response["total_work_experience"], 90),
-            "courses": self.match_records(ground_truth["courses"], generated_response["courses"]),
-            "publications": self.match_records(ground_truth["publications"], generated_response["publications"]),
-            "accomplishments": self.match_records(ground_truth["accomplishments"], generated_response["accomplishments"]),
-            "skills": self.match_records(ground_truth["skills"], generated_response["skills"]),
+            "courses": self.match_records(ground_truth["courses"], generated_response["courses"], 90),
+            "publications": self.match_records(ground_truth["publications"], generated_response["publications"], 90),
+            # TODO: Better way to get the accomplishments
+            # "accomplishments": self.match_records(ground_truth["accomplishments"], generated_response["accomplishments"], 90),
+            # TODO: If 95 % skills matched correctly then success
+            "skills": self.match_records(ground_truth["skills"], generated_response["skills"], 90),
             "expected_salary": self.hard_match(ground_truth["expected_salary"], generated_response["expected_salary"])
         }
 
@@ -65,3 +77,4 @@ if __name__ == '__main__':
 
     df = pd.DataFrame(rows)
     print(df)
+    df.to_csv('results/results2.csv')
--- structured_output/structured_output.py
+++ structured_output/structured_output.py
@@ -3,7 +3,7 @@ from typing import Optional, List
 
 class WorkDetails(BaseModel):
     company_name: Optional[str] = Field(None, description='Full name of the organization the candidate worked or currently works at (e.g., Google, Amazon). If no name is mentioned set to null.')
-    designation: str = Field(None, description='The job title or position held by the candidate at the specified company (e.g., Software Engineer, QA Lead).')
+    designation: Optional[str] = Field(None, description='The job title or position held by the candidate at the specified company (e.g., Software Engineer, QA Lead).')
     period: str = Field(None, description='The duration of employment at the company, ideally in the format "MM/YYYY to MM/YYYY" or "MM/YYYY or YYYY to Present or Current" if still employed.')
 
 class EducationDetails(BaseModel):
@@ -16,12 +16,12 @@ class ResumeChecklist(BaseModel):
     role: str = Field(..., description='Job role the candidate is applying for or the most relevant job title based on the resume content.')
     links: Optional[List[str]] = Field(None, description='Links attached in the resume like the link to GitHub / LinkedIn / Portfolio Website / Blogs...')
     professional_summary: str = Field(..., description='A brief summary of the candidate\'s career objectives, key skills, and professional focus.')
-    work_experience: Optional[List[WorkDetails]] = Field(None, description='Details of the most recent or relevant professional experience of the candidate, including company, designation, and duration.')
-    total_work_experience: str = Field(..., description='Total years of professional experience (e.g., "5", "10+" if currently employed). Append "+" if current role is ongoing.')
+    work_experience: Optional[List[WorkDetails]] = Field(None, description='Details of professional work experience of the candidate, including company, designation, and duration.')
+    total_work_experience: str = Field(..., description='Total years of professional experience (e.g., "5", "10+" if currently employed). If the candidate\'s last work experience is ongoing (e.g., the word \'current\' is mentioned), append a '+' to indicate it\'s still active. In that case, use the current year (2025) to calculate experience duration. Otherwise, calculate the duration using the end year provided.')
     education: Optional[List[EducationDetails]] = Field(None, description='Highest or most relevant educational qualification with institution and completion year.')
-    courses: Optional[List[str]] = Field(None, description='List of non-degree courses or certifications completed by the candidate (e.g., "Data Science Bootcamp", "Certified Scrum Master").')
+    courses: Optional[List[str]] = Field(None, description='List of non-degree courses or certifications completed by the candidate (e.g., "Data Science Bootcamp", "Certified Scrum Master"). Please note the education details are different from coursework done by the candidate.')
     publications: Optional[List[str]] = Field(None, description='List of academic or professional publications, if any, authored or co-authored by the candidate.')
     projects_count: Optional[int] = Field(None, description="Number of distinct projects mentioned or led in resume")
-    accomplishments: Optional[List[str]] = Field(None, description='Professional achievements, awards, recognitions, or notable contributions mentioned in the resume.')
-    skills: Optional[List[str]] = Field(None, description='Explicitly listed technical or soft skills (e.g., Python, Agile, Leadership) extracted from the resume.')
+    accomplishments: Optional[List[str]] = Field(None, description='Professional achievements, awards, recognitions, or notable contributions mentioned in the resume. This can be in an embedded in a paragraph too.')
+    skills: Optional[List[str]] = Field(None, description='Explicitly listed technical or soft skills (e.g., Python, Agile, Leadership) extracted from the resume. Note that this can  be listed under skills or skill highlights , if  both exist combine the content from both and only take the unique set of skills.')
     expected_salary: Optional[int] = Field(..., description='The salary the candidate expects to receive, ideally in the local currency and as an annual amount (e.g., 70000 for $70,000/year). If nothing is mentioned simply return 0.')
