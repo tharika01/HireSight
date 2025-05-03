37bb56ffa82397c19fdc59ca614d5d5e5d15559b
vikassrini <1rn20cs168.tharika@gmail.com>
2025-05-03 22:02:24 +0530

    refactor: add param and openai client

--- backend/configurations/openai_client_config.py
+++ backend/configurations/openai_client_config.py
@@ -1,10 +1,11 @@
-from openai import AzureOpenAI
+from openai import AzureOpenAI, OpenAI
 from backend.configurations.app_configurations import OPENAI_API_KEY, azure_endpoint, azure_openai_api_version, azure_openai_model, setup_logger
 
 logger = setup_logger()
 
-client = AzureOpenAI(
-            azure_endpoint=azure_endpoint,
-            api_key=OPENAI_API_KEY,
-            api_version=azure_openai_api_version
-        )
+# client = AzureOpenAI(
+#             azure_endpoint=azure_endpoint,
+#             api_key=OPENAI_API_KEY,
+#             api_version=azure_openai_api_version
+#         )
+client = OpenAI(api_key=OPENAI_API_KEY)
